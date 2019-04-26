9e76bcff291511a15b1335ae67fc70d2d1d24f4d
Veeck <veeck@neofonie.de>
2019-04-26 13:55:46 +0200

    Refactor app into components

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,30 @@
 <template>
   <div id="app">
-    <div>
-      <h5>Voice detection:</h5>
-      <p>{{ voice.confidence }} / {{ voice.detected }}</p>
-    </div>
+    <speak-to-me />
     <div class="wrapper">
-      <div
-        class="card"
+      <card
         v-for="card in cards"
         v-bind:key="card.id"
-        v-bind:class="{ active: !card.showFace }"
-        v-on:click="handleClick(card)"
-      >
-        <span class="text">{{ card.id }}</span>
-      </div>
+        v-bind:label="card.id"
+        v-bind:showFace="card.showFace"
+        v-bind:onClick="handleClick"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import SpeakToMe from "speaktome-api/build/stm_web.min.js";
+import SpeakToMe from "./SpeakToMe";
+import Card from "./Card";
 
 export default {
   name: "app",
 
+  components: { Card, SpeakToMe },
+
   data() {
     return {
       cards: [],
-
-      voice: {
-        confidence: "",
-        detected: ""
-      },
 
       matchingOptions: [
         { name: "Bier", pairs: 2 },
@@ -51,44 +44,21 @@ export default {
   },
 
   methods: {
-    activateVoice() {
-      const stm = SpeakToMe({
-        listener: msg => {
-          console.log("listener", msg);
-          if (msg.data && msg.data.length > 0) {
-            console.log(msg.data[0].confidence + ": " + msg.data[0].text);
-            if (msg.data[0].text === parseInt(msg.data[0].text).toString()) {
-              this.voice.confidence = msg.data[0].confidence;
-              this.voice.detected = msg.data[0].text;
-            }
-          }
-          if (msg.state === "ready") {
-            stm.listen();
-          }
-        }
-      });
-
-      stm.listen();
-    },
-
     populateBoard() {
       for (let i = 0; i <= 24; i++) {
         this.cards.push({ id: i, showFace: false });
       }
     },
 
-    handleClick(card) {
-      card.showFace = true;
-
-      setTimeout(function() {
-        card.showFace = false;
+    handleClick(id) {
+      this.cards[id].showFace = true;
+      setTimeout(() => {
+        this.cards[id].showFace = false;
       }, 2000);
     }
   },
 
   mounted() {
-    this.activateVoice();
-
     this.populateBoard();
   }
 };
@@ -107,31 +77,5 @@ export default {
   display: grid;
   grid-template-columns: repeat(5, 100px);
   grid-gap: 10px;
-
-  .card {
-    background-color: #444;
-    color: #fff;
-    border-radius: 5px;
-    padding: 20px;
-    font-size: 150%;
-    order: 1;
-    cursor: pointer;
-    height: 25px;
-    transition: all 0.4s ease;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .active {
-    color: #e51826;
-    background: #e51826;
-    transform: rotateY(180deg);
-
-    .text {
-      display: none;
-    }
-  }
 }
 </style>
---
+++ src/SpeakToMe.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h5>Voice detection:</h5>
+    <p>{{ voice.confidence }} / {{ voice.detected }}</p>
+  </div>
+</template>
+
+<script>
+import SpeakToMe from "speaktome-api/build/stm_web.min.js";
+
+export default {
+  name: "speak-to-me",
+
+  data() {
+    return {
+      voice: {
+        confidence: "",
+        detected: ""
+      }
+    };
+  },
+
+  mounted() {
+    const stm = SpeakToMe({
+      listener: msg => {
+        console.log("listener", msg);
+        if (msg.data && msg.data.length > 0) {
+          console.log(msg.data[0].confidence + ": " + msg.data[0].text);
+          if (msg.data[0].text === parseInt(msg.data[0].text).toString()) {
+            this.voice.confidence = msg.data[0].confidence;
+            this.voice.detected = msg.data[0].text;
+          }
+        }
+        if (msg.state === "ready") {
+          stm.listen();
+        }
+      }
+    });
+
+    stm.listen();
+  }
+};
+</script>
+
+<style scoped></style>
