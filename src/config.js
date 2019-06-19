a158b0d84d19b070b0c2db1f53cd87d55489568e
Veeck <veeck@neofonie.de>
2019-06-19 14:24:16 +0200

    Add new OctoCats images

--- src/config.js
+++ src/config.js
@@ -1,16 +1,10 @@
 export default {
   matchingOptions: [
-    { name: "Veeck", image: require("@/assets/people/veeck.jpg") },
-    { name: "Tim", image: require("@/assets/people/tim.png") },
-    { name: "Kai", image: require("@/assets/people/kai.png") },
-    { name: "Willy", image: require("@/assets/people/willy.jpg") },
-    { name: "Matthias", image: require("@/assets/people/matthias.jpg") },
-    { name: "Jenny", image: require("@/assets/people/jenny.png") },
-    { name: "Anja", image: require("@/assets/people/anja.jpg") },
-    { name: "Nicola", image: require("@/assets/people/nicola.jpg") },
-    { name: "Michal", image: require("@/assets/people/michal.jpg") },
-    { name: "Michael", image: require("@/assets/people/michael.jpg") },
-    { name: "Lennard", image: require("@/assets/people/lennard.jpg") },
-    { name: "Serkan", image: require("@/assets/people/serkan.jpg") }
+    { name: "Veeck", image: require("@/assets/people/octo_veeck.png") },
+    { name: "Tim", image: require("@/assets/people/octo_tim.png") },
+    { name: "Jenny", image: require("@/assets/people/octo_jenny.png") },
+    { name: "Anja", image: require("@/assets/people/octo_anja.png") },
+    { name: "Ale", image: require("@/assets/people/octo_ale.png") },
+    { name: "Matthias", image: require("@/assets/people/octo_matthias.png") }
   ]
 };
