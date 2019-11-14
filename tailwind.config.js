801940d1a0ddfc7d78eada6eb1f7ed962e518a1f
Veeck <veeck@neofonie.de>
2019-11-14 16:17:37 +0100

    Update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,66 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   theme: {
+    fontFamily: {
+      neo: ["Camingo", "Tahoma", "Arial"]
+    },
+    fontSize: {
+      base: "1.125rem",
+      lg: "1.25rem",
+      xl: "1.5rem",
+      "2xl": "1.75rem",
+      "6xl": "4.25rem",
+      ...defaultTheme.fontSize
+    },
     extend: {
       colors: {
-        "neo-red": "#e60024"
+        "neo-black": "#695F5F",
+        "neo-blue": "#3D9ACC",
+        "neo-grey": "#969191",
+        "neo-red": "#E60023"
       }
     }
   },
   variants: {},
-  plugins: []
+  plugins: [
+    function({ addBase, config }) {
+      addBase({
+        body: {
+          color: config("theme.colors.neo-black"),
+          fontFamily: config("theme.fontFamily.neo"),
+          fontSize: config("theme.fontSize.base")
+        },
+        h1: {
+          color: config("theme.colors.neo-red"),
+          fontSize: config("theme.fontSize.6xl")
+        },
+        h2: {
+          color: config("theme.colors.neo-red"),
+          fontSize: config("theme.fontSize.xl")
+        },
+        h3: {
+          color: config("theme.colors.neo-red"),
+          fontSize: config("theme.fontSize.xl")
+        },
+        h4: {
+          color: config("theme.colors.neo-red"),
+          fontSize: config("theme.fontSize.xl")
+        },
+        h5: {
+          color: config("theme.colors.neo-red"),
+          fontSize: config("theme.fontSize.2xl")
+        },
+        a: {
+          color: config("theme.colors.neo-blue")
+        },
+        "a:hover": {
+          color: config("theme.colors.neo-red")
+        },
+        button: {
+          fontSize: config("theme.fontSize.xl")
+        }
+      });
+    }
+  ]
 };
