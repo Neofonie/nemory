8948c8245aaf9944524c503da1d5ba5c30bf436c
Veeck <veeck@neofonie.de>
2019-11-15 10:18:01 +0100

    Add purgecss for smaller style size

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+
 module.exports = {
   plugins: [
     require("tailwindcss")("tailwind.config.js"),
-    require("autoprefixer")()
+    require("autoprefixer")(),
+
+    // Only add purgecss in production
+    process.env.NODE_ENV === "production"
+      ? purgecss({
+          content: ["./src/**/*.html", "./src/**/*.vue"]
+        })
+      : ""
   ]
 };
