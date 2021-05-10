ce33229d697f4fa4e101f5788c4ff53c59ce9c7d
veeck <veeck@neofonie.de>
2021-05-10 21:12:34 +0200

    Fix image loading

--- nemory.config.js
+++ nemory.config.js
@@ -1,10 +1,10 @@
 export default {
   matchingOptions: [
-    { name: "Veeck", image: import("./src/assets/people/octo_veeck.png") },
-    { name: "Tim", image: import("./src/assets/people/octo_tim.png") },
-    { name: "Lisa", image: import("./src/assets/people/octo_lisa.png") },
-    { name: "Anja", image: import("./src/assets/people/octo_anja.png") },
-    { name: "Ale", image: import("./src/assets/people/octo_ale.png") },
-    { name: "Willy", image: import("./src/assets/people/octo_willy.png") }
+    { name: "Veeck", image: "./src/assets/people/octo_veeck.png"},
+    { name: "Tim", image: "./src/assets/people/octo_tim.png"},
+    { name: "Lisa", image: "./src/assets/people/octo_lisa.png"},
+    { name: "Anja", image: "./src/assets/people/octo_anja.png"},
+    { name: "Ale", image: "./src/assets/people/octo_ale.png"},
+    { name: "Willy", image: "./src/assets/people/octo_willy.png"}
   ]
 };
